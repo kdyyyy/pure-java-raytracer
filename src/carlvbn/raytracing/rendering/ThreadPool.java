ef1094829fe773153bbf5d81a05a408d49370172
21129840 <21129840@ppti-14-401-01.ufr-info-p6.jussieu.fr>
2024-10-16 17:43:21 +0200

    tme6

---
+++ src/carlvbn/raytracing/rendering/ThreadPool.java
@@ -0,0 +1,48 @@
+package carlvbn.raytracing.rendering;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ThreadPool {
+	private ArrayBlockingQueue<Runnable> queu;
+	private List<Thread> threads = new ArrayList<>();
+
+	private class PoolWorker implements Runnable {
+		public void run() {
+
+			try {
+				while (!Thread.interrupted()) {
+					Runnable r = queu.take();
+					r.run();
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public ThreadPool(int sizeq, int N) {
+		queu = new ArrayBlockingQueue<Runnable>(sizeq);
+		for (int i = 0; i < N; i++) {
+			Thread t = new Thread(new PoolWorker());
+			t.start();
+			threads.add(t);
+		}
+		for (Thread t : threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void submit(Runnable r) throws InterruptedException {
+		queu.add(r);
+
+	}
+
+}
